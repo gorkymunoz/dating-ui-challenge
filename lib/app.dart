4194df13782331e93b44a13e86de20dea4d2f465
Gorky Muñoz <gorkymunoz@hotmail.com>
2023-03-17 13:17:18 -0500

    show match card

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
-import 'package:dating_ui_challenge/screens/molecules/match_card.dart';
+import 'package:dating_ui_challenge/ui/molecules/explore_people_card.dart';
 import 'package:flutter/material.dart';
+
 
 class DatingApp extends StatelessWidget {
   const DatingApp({super.key});
@@ -14,9 +15,7 @@ class DatingApp extends StatelessWidget {
       ),
       home: const Scaffold(
         body: Center(
-          child: MatchPercentageCard(
-            matchPercentage: 50,
-          ),
+          child: ExplorePeopleCard(),
         ),
       ),
     );
