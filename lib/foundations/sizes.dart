71d104ac4b022f981573bfbfb8fa6d1ccda57c07
Gorky Muñoz <gorkymunoz@hotmail.com>
2023-03-17 13:16:23 -0500

    add foundations for widgets

--- lib/foundations/sizes.dart
+++ lib/foundations/sizes.dart
@@ -1,8 +1,9 @@
-import 'package:dating_ui_challenge/screens/tokens/sizes.dart';
+import 'package:dating_ui_challenge/ui/tokens/sizes.dart';
 
 class DatingSizes{
   DatingSizes._();
 
   static const double iconSize = Sizes.sizeS;
   static const double percentageCardSize = Sizes.sizeL;
+  static const double h1Size = Sizes.sizeXM;
 }
--- lib/foundations/spacing.dart
+++ lib/foundations/spacing.dart
@@ -1,8 +1,9 @@
-import 'package:dating_ui_challenge/screens/tokens/spacing.dart';
+import 'package:dating_ui_challenge/ui/tokens/spacing.dart';
 
 class DatingSpaces {
   DatingSpaces._();
 
   static const double paddingXS = Spacing.spaceSM;
   static const double borderPercentageCard = Spacing.spaceLG;
+  static const double borderMatchPhoto = Spacing.spaceSL;
 }
