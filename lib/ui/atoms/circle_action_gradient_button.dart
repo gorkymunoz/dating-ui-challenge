3178d5b2c7d702739d20b8e8825ba3af81dffcfc
Gorky Muñoz <gorkymunoz@hotmail.com>
2023-04-17 16:50:47 -0500

    add circle button with gradient

---
+++ lib/ui/atoms/circle_action_gradient_button.dart
@@ -0,0 +1,56 @@
+import 'package:dating_ui_challenge/foundations/colors.dart';
+import 'package:flutter/material.dart';
+
+class CircleButtonGradientProperties {
+  final List<Color> colors;
+  final Alignment begin;
+  final Alignment end;
+
+  CircleButtonGradientProperties(
+      {required this.colors,
+      this.begin = Alignment.topLeft,
+      this.end = Alignment.bottomRight});
+}
+
+class CircleActionGradientButton extends StatelessWidget {
+  final double iconSize;
+  final IconData icon;
+  final CircleButtonGradientProperties gradientProperties;
+  final VoidCallback onPressed;
+
+  const CircleActionGradientButton(
+      {Key? key,
+      required this.iconSize,
+      required this.icon,
+      required this.gradientProperties,
+      required this.onPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: DatingColors.colorOnSurface,
+          border: Border.all(color: DatingColors.colorGray.withAlpha(25)),
+          boxShadow: const [
+            BoxShadow(color: DatingColors.colorGray, blurRadius: 25)
+          ]),
+      child: ShaderMask(
+        shaderCallback: (Rect bounds) {
+          return LinearGradient(
+                  colors: gradientProperties.colors,
+                  begin: gradientProperties.begin,
+                  end: gradientProperties.end)
+              .createShader(bounds);
+        },
+        child: IconButton(
+          iconSize: iconSize,
+          icon: Icon(icon),
+          color: Colors.white,
+          onPressed: onPressed,
+        ),
+      ),
+    );
+  }
+}
