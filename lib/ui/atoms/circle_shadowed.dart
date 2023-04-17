d913191c584e6964dc37fc5118ba9f62cc7a5912
Gorky Muñoz <gorkymunoz@hotmail.com>
2023-04-17 17:00:36 -0500

    add circle shadowed container

---
+++ lib/ui/atoms/circle_shadowed.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'package:dating_ui_challenge/foundations/colors.dart';
+
+class CircleShadowed extends StatelessWidget {
+  final Widget children;
+
+  const CircleShadowed({Key? key, required this.children}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: DatingColors.colorOnSurface,
+            border: Border.all(color: DatingColors.colorGray.withAlpha(25)),
+            boxShadow: const [
+              BoxShadow(color: DatingColors.colorGray, blurRadius: 25)
+            ]),
+        child: children);
+  }
+}
