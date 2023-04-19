301d6dff2d7d8296c2e515e18aba51189bebeba1
Gorky Muñoz <gorkymunoz@hotmail.com>
2023-04-19 10:56:29 -0500

    add provider to the tree

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'package:dating_ui_challenge/domain/providers/InfomatchCardProvider.dart';
 import 'package:dating_ui_challenge/ui/organisms/info_match_card.dart';
 
 class DatingApp extends StatelessWidget {
@@ -8,14 +10,19 @@ class DatingApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Scaffold(
-        body: Center(
-          child: InfoMatchCard(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => InfoMatchCardProvider())
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const Scaffold(
+          body: Center(
+            child: InfoMatchCard(),
+          ),
         ),
       ),
     );
